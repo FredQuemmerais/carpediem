8a1b906dbf8488a220d6f42078d4ce050d0af312
AlexisEsquerre <alexis.esquerre7@gmail.com>
2026-02-05 16:13:22 +0100

    Revert "Ajout des nouveaux fichiers"
    This reverts commit a3299b4148a66d092728338553394470bc14fb10.

--- FONCTIONS/f_Int_to_Log.R
+++
@@ -1,8 +0,0 @@
-# -------------------------------------------------------------
-# Mini fonction pour transformer valeurs entieres en logiques pour le fichier de paramètres
-# Assure que tout remplissage mal fait du fichier résulte en un NA, pas en un TRUE
-# Note : as.logical() décide que tout ce qui n'est pas 0 (FALSE) est forcément TRUE.
-# -------------------------------------------------------------
-f_Int_to_Log <- function(x){
-  ifelse(x == 0, FALSE, ifelse(x == 1, TRUE, NA))
-}
